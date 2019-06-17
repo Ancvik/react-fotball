75152331d21f7ffde0e657d48a0fe0c6af847849
Ancvik <stian.angvik92@gmail.com>
2019-06-17 12:26:20 +0200

    enda mer styling

--- src/App.js
+++ src/App.js
@@ -18,7 +18,10 @@ class App extends React.Component {
           <Route path="/" exact component={Table} />
           <Route path="/team/:teamId" component={Team} />
           <Route path="/match/:matchId" component={Match} />
-          <Route path="/matchstats/:matchId" component={StatsForMatch} />         
+          <Route path="/matchstats/:matchId" component={StatsForMatch} /> 
+          <ul className="info">
+              <li className="info">djad dsas da fasf afsf </li>
+            </ul>      
          
         </div>
 
